73f9594099d8964b13f7e4f8c9462567c24b62ca
StrategicVirus <107707720+StrategicVirus@users.noreply.github.com>
2024-11-06 18:11:05 +0000

    Add files via upload

---
+++ graphics/Maze.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Maze" tilewidth="32" tileheight="32" tilecount="12" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image source="../../Tiled - tilemap creator/individual images for tilemap/maze/png files/characters/Chara1_backwards_anim_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="../../Tiled - tilemap creator/individual images for tilemap/maze/png files/characters/Chara1_backwards_anim_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="../../Tiled - tilemap creator/individual images for tilemap/maze/png files/characters/Chara1_backwards_anim_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="../../Tiled - tilemap creator/individual images for tilemap/maze/png files/characters/Chara1_forwards_anim_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="../../Tiled - tilemap creator/individual images for tilemap/maze/png files/characters/Chara1_forwards_anim_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="../../Tiled - tilemap creator/individual images for tilemap/maze/png files/characters/Chara1_forwards_anim_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="../../Tiled - tilemap creator/individual images for tilemap/maze/png files/characters/Chara1_left_anim_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="8">
+  <image source="../../Tiled - tilemap creator/individual images for tilemap/maze/png files/characters/Chara1_left_anim_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="9">
+  <image source="../../Tiled - tilemap creator/individual images for tilemap/maze/png files/characters/Chara1_left_anim_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="12">
+  <image source="../../Tiled - tilemap creator/individual images for tilemap/maze/png files/characters/Chara1_right_anim1.png" width="32" height="32"/>
+ </tile>
+ <tile id="10">
+  <image source="../../Tiled - tilemap creator/individual images for tilemap/maze/png files/characters/Chara1_right_anim_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="11">
+  <image source="../../Tiled - tilemap creator/individual images for tilemap/maze/png files/characters/Chara1_right_anim_3.png" width="32" height="32"/>
+ </tile>
+</tileset>
